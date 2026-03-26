b29003318ca82cab58d8ed227649398f490dc2ff
Philip Gonzales <philip08533gonzales@gmail.com>
2026-03-25 23:12:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10786911
+    "zoneId": 10786950
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
